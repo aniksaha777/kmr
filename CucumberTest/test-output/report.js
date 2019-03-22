ef977f446bb2d611d7d6e01ab3b18dca0b63db3d
SAMMSALAPTOP1 <SAMMSALAPTOP1@DESKTOP-9QM2DH8>
2019-03-22 14:43:00 +0530

    update@featurefile

--- CucumberTest/test-output/report.js
+++ CucumberTest/test-output/report.js
@@ -8,11 +8,21 @@ formatter.feature({
 });
 formatter.scenarioOutline({
   "line": 3,
-  "name": "Local 13 Login Scenario",
+  "name": "Local 13 Successful Login Scenario",
   "description": "",
-  "id": "local-13-login-feature;local-13-login-scenario",
+  "id": "local-13-login-feature;local-13-successful-login-scenario",
   "type": "scenario_outline",
-  "keyword": "Scenario Outline"
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@SmokeTest"
+    },
+    {
+      "line": 2,
+      "name": "@RegressionTest"
+    }
+  ]
 });
 formatter.step({
   "line": 4,
@@ -40,37 +50,47 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.examples({
-  "line": 11,
+  "line": 9,
   "name": "",
   "description": "",
-  "id": "local-13-login-feature;local-13-login-scenario;",
+  "id": "local-13-login-feature;local-13-successful-login-scenario;",
   "rows": [
     {
       "cells": [
         "username",
         "Password"
       ],
-      "line": 12,
-      "id": "local-13-login-feature;local-13-login-scenario;;1"
+      "line": 10,
+      "id": "local-13-login-feature;local-13-successful-login-scenario;;1"
     },
     {
       "cells": [
         "mgg",
         "mgg1105"
       ],
-      "line": 13,
-      "id": "local-13-login-feature;local-13-login-scenario;;2"
+      "line": 11,
+      "id": "local-13-login-feature;local-13-successful-login-scenario;;2"
     }
   ],
   "keyword": "Examples"
 });
 formatter.scenario({
-  "line": 13,
-  "name": "Local 13 Login Scenario",
+  "line": 11,
+  "name": "Local 13 Successful Login Scenario",
   "description": "",
-  "id": "local-13-login-feature;local-13-login-scenario;;2",
+  "id": "local-13-login-feature;local-13-successful-login-scenario;;2",
   "type": "scenario",
-  "keyword": "Scenario Outline"
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@RegressionTest"
+    },
+    {
+      "line": 2,
+      "name": "@SmokeTest"
+    }
+  ]
 });
 formatter.step({
   "line": 4,
@@ -105,14 +125,14 @@ formatter.match({
   "location": "LoginSteps.user_is_on_Login_Page()"
 });
 formatter.result({
-  "duration": 11372601221,
+  "duration": 7596197324,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.the_title_of_Login_page_is_Login()"
 });
 formatter.result({
-  "duration": 29356716,
+  "duration": 7993569,
   "status": "passed"
 });
 formatter.match({
@@ -129,21 +149,174 @@ formatter.match({
   "location": "LoginSteps.user_enters_username_and_Password(String,String)"
 });
 formatter.result({
-  "duration": 121843117,
+  "duration": 114434482,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_Clicks_on_Login_button()"
 });
 formatter.result({
-  "duration": 10319423147,
+  "duration": 12466615159,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_is_on_Dashboard()"
 });
 formatter.result({
-  "duration": 30613582,
+  "duration": 28376910,
   "status": "passed"
+});
+formatter.scenarioOutline({
+  "line": 14,
+  "name": "Local 13 Negative Login Scenario",
+  "description": "",
+  "id": "local-13-login-feature;local-13-negative-login-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 13,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "User is on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "the title of Login page is Login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "User enters \"\u003cusername\u003e\" and \"\u003cPassword\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "User Clicks on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "User is on Dashboard",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 20,
+  "name": "",
+  "description": "",
+  "id": "local-13-login-feature;local-13-negative-login-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "Password"
+      ],
+      "line": 21,
+      "id": "local-13-login-feature;local-13-negative-login-scenario;;1"
+    },
+    {
+      "cells": [
+        "mgg",
+        "mgg1101"
+      ],
+      "line": 22,
+      "id": "local-13-login-feature;local-13-negative-login-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "Local 13 Negative Login Scenario",
+  "description": "",
+  "id": "local-13-login-feature;local-13-negative-login-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 13,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "User is on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "the title of Login page is Login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "User enters \"mgg\" and \"mgg1101\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "User Clicks on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "User is on Dashboard",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.user_is_on_Login_Page()"
+});
+formatter.result({
+  "duration": 7287545353,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginSteps.the_title_of_Login_page_is_Login()"
+});
+formatter.result({
+  "duration": 6152220,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "mgg",
+      "offset": 13
+    },
+    {
+      "val": "mgg1101",
+      "offset": 23
+    }
+  ],
+  "location": "LoginSteps.user_enters_username_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 110733794,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginSteps.user_Clicks_on_Login_button()"
+});
+formatter.result({
+  "duration": 592424668,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginSteps.user_is_on_Dashboard()"
+});
+formatter.result({
+  "duration": 20108322,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//*[@title\u003d\"Log Out\"]\"}\n  (Session info: chrome\u003d72.0.3626.121)\n  (Driver info: chromedriver\u003d2.45.615291 (ec3682e3c9061c10f26ea9e5cdcf3c53f3f74387),platform\u003dWindows NT 10.0.17134 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.5.3\u0027, revision: \u0027a88d25fe6b\u0027, time: \u00272017-08-29T12:42:44.417Z\u0027\nSystem info: host: \u0027DESKTOP-9QM2DH8\u0027, ip: \u0027192.168.137.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002710.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities [{mobileEmulationEnabled\u003dfalse, timeouts\u003d{implicit\u003d0, pageLoad\u003d300000, script\u003d30000}, hasTouchScreen\u003dfalse, platform\u003dXP, acceptSslCerts\u003dfalse, goog:chromeOptions\u003d{debuggerAddress\u003dlocalhost:55882}, acceptInsecureCerts\u003dfalse, webStorageEnabled\u003dtrue, browserName\u003dchrome, takesScreenshot\u003dtrue, javascriptEnabled\u003dtrue, platformName\u003dXP, setWindowRect\u003dtrue, unexpectedAlertBehaviour\u003dignore, applicationCacheEnabled\u003dfalse, rotatable\u003dfalse, networkConnectionEnabled\u003dfalse, chrome\u003d{chromedriverVersion\u003d2.45.615291 (ec3682e3c9061c10f26ea9e5cdcf3c53f3f74387), userDataDir\u003dC:\\Users\\SAMMSA~1\\AppData\\Local\\Temp\\scoped_dir17264_27281}, takesHeapSnapshot\u003dtrue, unhandledPromptBehavior\u003dignore, pageLoadStrategy\u003dnormal, strictFileInteractability\u003dfalse, databaseEnabled\u003dfalse, handlesAlerts\u003dtrue, version\u003d72.0.3626.121, browserConnectionEnabled\u003dfalse, proxy\u003dProxy(), nativeEvents\u003dtrue, locationContextEnabled\u003dtrue, cssSelectorsEnabled\u003dtrue}]\nSession ID: 21a57716e71bc7873b2b41d329019666\n*** Element info: {Using\u003dxpath, value\u003d//*[@title\u003d\"Log Out\"]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:215)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:167)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:82)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:45)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:82)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:646)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:416)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:518)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:361)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:408)\r\n\tat StepDefinations.LoginSteps.user_is_on_Dashboard(LoginSteps.java:47)\r\n\tat ✽.Then User is on Dashboard(Login.feature:19)\r\n",
+  "status": "failed"
 });
 });

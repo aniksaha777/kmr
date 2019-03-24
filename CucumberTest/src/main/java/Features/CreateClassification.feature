707644f70c1631ec91fd15f256a5a3fce0c8b913
SAMMSALAPTOP1 <SAMMSALAPTOP1@192.168.0.122>
2019-03-24 11:32:40 +0530

    test

--- CucumberTest/src/main/java/Features/CreateClassification.feature
+++ CucumberTest/src/main/java/Features/CreateClassification.feature
@@ -1,10 +1,9 @@
 Feature: Local 13 Manage Classification
  @SmokeTest
- Scenario Outline: Manage Classification - Create Classification
+ Scenario: Manage Classification - Create Classification
     Given The User is logged in
     When User is on Apps Builder Create Classification Page
     And  the user enters valid data and clicks on Submit
     | Classification_Group | Classification_Code | Classification_Desc | Short | Wages |
     | Apprentice | BTAB1 | Building Trade Apprentice Pipefitter | AB12 | 33.70 |
-    
-    Then new classification is displayed in grid
+    Then new classification is displayed in grid 
--- CucumberTest/src/main/java/StepDefinations/CreateClassification.java
+++ CucumberTest/src/main/java/StepDefinations/CreateClassification.java
@@ -9,5 +9,50 @@ import junit.framework.Assert;
 @SuppressWarnings("deprecation")
 
 public class CreateClassification {
+	
+	WebDriver driver;
+	@Given("^The User is logged in$")
+	public void the_User_is_logged_in() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SAMMSALAPTOP1\\Desktop\\AutomationDrivers\\chromedriver_win32\\chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.get("https://ua13.kmrapplications.com/local13test/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+	    Assert.assertEquals("Login | Local-13", title);
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("mgg");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("mgg1105");
+		driver.findElement(By.xpath("//*[@id=\"submit\"]")).click(); 
+		boolean isPresent = driver.findElement(By.xpath("//*[@title=\"Log Out\"]")).isDisplayed();
+		if(isPresent= true) {
+			System.out.println("User is Logged in Successfully");
+		}
+		else {
+				System.out.println("User is Not Logged In");	
+		}
+	}
+
+	@When("^User is on Apps Builder Create Classification Page$")
+	public void user_is_on_Apps_Builder_Create_Classification_Page() {
+		driver.findElement(By.xpath("//*[@id=\"build_a_2\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"build_div_2\"]//button[2]/img")).click();
+		driver.findElement(By.xpath("//*[text()=\"Manage Classification\"]")).click();
+		driver.findElement(By.xpath("//*[@title=\"Open West Pane\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"function_111\"]//a")).click();
+		driver.switchTo().frame("functionContent");
+		
+		
+	    
+	    
+	}
+
+	@When("^the user enters valid data and clicks on Submit$")
+	public void the_user_enters_valid_data_and_clicks_on_Submit() {
+
+	}
+
+	@Then("^new classification is displayed in grid$")
+	public void new_classification_is_displayed_in_grid() {
+	    // Write code here that turns the phrase above into concrete actions
+	}
 
 }
